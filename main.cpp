5727d015f55e05766624a90290dd290c7b0fa8c2
Léon <leon.surbeck@gmail.com>
2022-12-13 10:34:48 +0100

    1rt

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/*
+-----------------------------------------------------------------------------------
+Nom du fichier  : main.cpp
+Auteur(s)       : Ouweis Harun, Léon Surbeck
+Classe          : PRG1B
+Date creation   : 13.12.2022
+Description     :
+Remarque(s)     : -
+Compilateur     : Mingw-w64 g++ 12.2.0
+-----------------------------------------------------------------------------------
+*/
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main() {
+
+ return EXIT_SUCCESS;
+}
